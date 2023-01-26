9f395f6a901ecb75c451e9ee646f0e18521f6865
Matus Jankech <jankech.matus@gmail.com>
2023-01-27 00:07:00 +0100

    Serial communication test successful ?!

--- PID_Tuner_App/PID_Tuner_App/Form1.Designer.cs
+++ PID_Tuner_App/PID_Tuner_App/Form1.Designer.cs
@@ -38,15 +38,16 @@
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.SerialPanel = new System.Windows.Forms.Panel();
             this.SerialComLabel = new System.Windows.Forms.Label();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.P_VelText = new System.Windows.Forms.TextBox();
+            this.I_VelText = new System.Windows.Forms.TextBox();
+            this.D_VelText = new System.Windows.Forms.TextBox();
             this.PLabel = new System.Windows.Forms.Label();
             this.ILabel = new System.Windows.Forms.Label();
             this.DLabel = new System.Windows.Forms.Label();
             this.VelocityPanel = new System.Windows.Forms.Panel();
             this.VelocityLabel = new System.Windows.Forms.Label();
+            this.VelSendButton = new System.Windows.Forms.Button();
+            this.VelReadButton = new System.Windows.Forms.Button();
             this.SerialPanel.SuspendLayout();
             this.VelocityPanel.SuspendLayout();
             this.SuspendLayout();
@@ -55,9 +56,10 @@
             // 
             this.PortMenu.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.PortMenu.FormattingEnabled = true;
-            this.PortMenu.Location = new System.Drawing.Point(81, 46);
+            this.PortMenu.Location = new System.Drawing.Point(108, 57);
+            this.PortMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PortMenu.Name = "PortMenu";
-            this.PortMenu.Size = new System.Drawing.Size(121, 21);
+            this.PortMenu.Size = new System.Drawing.Size(160, 24);
             this.PortMenu.TabIndex = 0;
             this.PortMenu.SelectedIndexChanged += new System.EventHandler(this.PortMenu_SelectedIndexChanged);
             this.PortMenu.Click += new System.EventHandler(this.PortMenu_Click);
@@ -66,9 +68,10 @@
             // 
             this.PortLabel.AutoSize = true;
             this.PortLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.PortLabel.Location = new System.Drawing.Point(19, 49);
+            this.PortLabel.Location = new System.Drawing.Point(25, 60);
+            this.PortLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.PortLabel.Name = "PortLabel";
-            this.PortLabel.Size = new System.Drawing.Size(56, 13);
+            this.PortLabel.Size = new System.Drawing.Size(73, 17);
             this.PortLabel.TabIndex = 1;
             this.PortLabel.Text = "COM Port:";
             // 
@@ -76,9 +79,10 @@
             // 
             this.BaudRateLabel.AutoSize = true;
             this.BaudRateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.BaudRateLabel.Location = new System.Drawing.Point(14, 79);
+            this.BaudRateLabel.Location = new System.Drawing.Point(19, 97);
+            this.BaudRateLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.BaudRateLabel.Name = "BaudRateLabel";
-            this.BaudRateLabel.Size = new System.Drawing.Size(61, 13);
+            this.BaudRateLabel.Size = new System.Drawing.Size(79, 17);
             this.BaudRateLabel.TabIndex = 2;
             this.BaudRateLabel.Text = "Baud Rate:";
             // 
@@ -93,9 +97,10 @@
             "38400",
             "57600",
             "115200"});
-            this.BaudRateMenu.Location = new System.Drawing.Point(81, 76);
+            this.BaudRateMenu.Location = new System.Drawing.Point(108, 94);
+            this.BaudRateMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BaudRateMenu.Name = "BaudRateMenu";
-            this.BaudRateMenu.Size = new System.Drawing.Size(121, 21);
+            this.BaudRateMenu.Size = new System.Drawing.Size(160, 24);
             this.BaudRateMenu.TabIndex = 3;
             this.BaudRateMenu.SelectedIndexChanged += new System.EventHandler(this.BaudRateMenu_SelectedIndexChanged);
             // 
@@ -103,9 +108,10 @@
             // 
             this.ConnectButton.BackColor = System.Drawing.Color.Lime;
             this.ConnectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ConnectButton.Location = new System.Drawing.Point(17, 113);
+            this.ConnectButton.Location = new System.Drawing.Point(23, 139);
+            this.ConnectButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ConnectButton.Name = "ConnectButton";
-            this.ConnectButton.Size = new System.Drawing.Size(185, 23);
+            this.ConnectButton.Size = new System.Drawing.Size(247, 28);
             this.ConnectButton.TabIndex = 4;
             this.ConnectButton.Text = "Connect";
             this.ConnectButton.UseVisualStyleBackColor = false;
@@ -116,13 +122,18 @@
             this.DisconnectButton.BackColor = System.Drawing.Color.Red;
             this.DisconnectButton.Enabled = false;
             this.DisconnectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.DisconnectButton.Location = new System.Drawing.Point(17, 113);
+            this.DisconnectButton.Location = new System.Drawing.Point(23, 139);
+            this.DisconnectButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DisconnectButton.Name = "DisconnectButton";
-            this.DisconnectButton.Size = new System.Drawing.Size(185, 23);
+            this.DisconnectButton.Size = new System.Drawing.Size(247, 28);
             this.DisconnectButton.TabIndex = 5;
             this.DisconnectButton.Text = "Disconnect";
             this.DisconnectButton.UseVisualStyleBackColor = false;
             this.DisconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
             // 
             // SerialPanel
             // 
@@ -135,9 +146,10 @@
             this.SerialPanel.Controls.Add(this.PortLabel);
             this.SerialPanel.Controls.Add(this.PortMenu);
             this.SerialPanel.Controls.Add(this.BaudRateLabel);
-            this.SerialPanel.Location = new System.Drawing.Point(26, 26);
+            this.SerialPanel.Location = new System.Drawing.Point(35, 32);
+            this.SerialPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SerialPanel.Name = "SerialPanel";
-            this.SerialPanel.Size = new System.Drawing.Size(228, 149);
+            this.SerialPanel.Size = new System.Drawing.Size(303, 183);
             this.SerialPanel.TabIndex = 6;
             // 
             // SerialComLabel
@@ -147,74 +159,84 @@
             this.SerialComLabel.BackColor = System.Drawing.Color.LightBlue;
             this.SerialComLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.SerialComLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.SerialComLabel.Location = new System.Drawing.Point(9, 9);
+            this.SerialComLabel.Location = new System.Drawing.Point(12, 11);
+            this.SerialComLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.SerialComLabel.Name = "SerialComLabel";
-            this.SerialComLabel.Padding = new System.Windows.Forms.Padding(3);
-            this.SerialComLabel.Size = new System.Drawing.Size(205, 24);
+            this.SerialComLabel.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.SerialComLabel.Size = new System.Drawing.Size(252, 30);
             this.SerialComLabel.TabIndex = 7;
             this.SerialComLabel.Text = "Serial communication setup";
             // 
-            // textBox1
+            // P_VelText
             // 
-            this.textBox1.Location = new System.Drawing.Point(46, 47);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(69, 20);
-            this.textBox1.TabIndex = 7;
+            this.P_VelText.Location = new System.Drawing.Point(56, 55);
+            this.P_VelText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.P_VelText.Name = "P_VelText";
+            this.P_VelText.Size = new System.Drawing.Size(91, 22);
+            this.P_VelText.TabIndex = 7;
             // 
-            // textBox2
+            // I_VelText
             // 
-            this.textBox2.Location = new System.Drawing.Point(46, 74);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(69, 20);
-            this.textBox2.TabIndex = 8;
+            this.I_VelText.Location = new System.Drawing.Point(56, 88);
+            this.I_VelText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.I_VelText.Name = "I_VelText";
+            this.I_VelText.Size = new System.Drawing.Size(91, 22);
+            this.I_VelText.TabIndex = 8;
             // 
-            // textBox3
+            // D_VelText
             // 
-            this.textBox3.Location = new System.Drawing.Point(46, 100);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(69, 20);
-            this.textBox3.TabIndex = 9;
+            this.D_VelText.Location = new System.Drawing.Point(56, 118);
+            this.D_VelText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.D_VelText.Name = "D_VelText";
+            this.D_VelText.Size = new System.Drawing.Size(91, 22);
+            this.D_VelText.TabIndex = 9;
             // 
             // PLabel
             // 
             this.PLabel.AutoSize = true;
-            this.PLabel.Location = new System.Drawing.Point(17, 50);
+            this.PLabel.Location = new System.Drawing.Point(23, 58);
+            this.PLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.PLabel.Name = "PLabel";
-            this.PLabel.Size = new System.Drawing.Size(23, 13);
+            this.PLabel.Size = new System.Drawing.Size(26, 16);
             this.PLabel.TabIndex = 10;
             this.PLabel.Text = "Kp:";
             // 
             // ILabel
             // 
             this.ILabel.AutoSize = true;
-            this.ILabel.Location = new System.Drawing.Point(21, 77);
+            this.ILabel.Location = new System.Drawing.Point(28, 91);
+            this.ILabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ILabel.Name = "ILabel";
-            this.ILabel.Size = new System.Drawing.Size(19, 13);
+            this.ILabel.Size = new System.Drawing.Size(21, 16);
             this.ILabel.TabIndex = 11;
             this.ILabel.Text = "Ki:";
             // 
             // DLabel
             // 
             this.DLabel.AutoSize = true;
-            this.DLabel.Location = new System.Drawing.Point(17, 103);
+            this.DLabel.Location = new System.Drawing.Point(23, 123);
+            this.DLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.DLabel.Name = "DLabel";
-            this.DLabel.Size = new System.Drawing.Size(23, 13);
+            this.DLabel.Size = new System.Drawing.Size(26, 16);
             this.DLabel.TabIndex = 12;
             this.DLabel.Text = "Kd:";
             // 
             // VelocityPanel
             // 
             this.VelocityPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.VelocityPanel.Controls.Add(this.VelReadButton);
+            this.VelocityPanel.Controls.Add(this.VelSendButton);
             this.VelocityPanel.Controls.Add(this.VelocityLabel);
             this.VelocityPanel.Controls.Add(this.PLabel);
             this.VelocityPanel.Controls.Add(this.DLabel);
-            this.VelocityPanel.Controls.Add(this.textBox1);
+            this.VelocityPanel.Controls.Add(this.P_VelText);
             this.VelocityPanel.Controls.Add(this.ILabel);
-            this.VelocityPanel.Controls.Add(this.textBox2);
-            this.VelocityPanel.Controls.Add(this.textBox3);
-            this.VelocityPanel.Location = new System.Drawing.Point(277, 26);
+            this.VelocityPanel.Controls.Add(this.I_VelText);
+            this.VelocityPanel.Controls.Add(this.D_VelText);
+            this.VelocityPanel.Location = new System.Drawing.Point(369, 32);
+            this.VelocityPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.VelocityPanel.Name = "VelocityPanel";
-            this.VelocityPanel.Size = new System.Drawing.Size(144, 149);
+            this.VelocityPanel.Size = new System.Drawing.Size(174, 200);
             this.VelocityPanel.TabIndex = 13;
             // 
             // VelocityLabel
@@ -224,20 +246,48 @@
             this.VelocityLabel.BackColor = System.Drawing.Color.LightBlue;
             this.VelocityLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.VelocityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.VelocityLabel.Location = new System.Drawing.Point(18, 9);
+            this.VelocityLabel.Location = new System.Drawing.Point(26, 11);
+            this.VelocityLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.VelocityLabel.Name = "VelocityLabel";
-            this.VelocityLabel.Padding = new System.Windows.Forms.Padding(3);
-            this.VelocityLabel.Size = new System.Drawing.Size(100, 24);
+            this.VelocityLabel.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.VelocityLabel.Size = new System.Drawing.Size(123, 30);
             this.VelocityLabel.TabIndex = 14;
             this.VelocityLabel.Text = "Velocity PID";
             // 
+            // VelSendButton
+            // 
+            this.VelSendButton.BackColor = System.Drawing.Color.Lime;
+            this.VelSendButton.Enabled = false;
+            this.VelSendButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.VelSendButton.Location = new System.Drawing.Point(88, 154);
+            this.VelSendButton.Name = "VelSendButton";
+            this.VelSendButton.Size = new System.Drawing.Size(59, 28);
+            this.VelSendButton.TabIndex = 15;
+            this.VelSendButton.Text = "Send";
+            this.VelSendButton.UseVisualStyleBackColor = false;
+            this.VelSendButton.Click += new System.EventHandler(this.VelSendButton_Click);
+            // 
+            // VelReadButton
+            // 
+            this.VelReadButton.BackColor = System.Drawing.Color.Red;
+            this.VelReadButton.Enabled = false;
+            this.VelReadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.VelReadButton.Location = new System.Drawing.Point(26, 154);
+            this.VelReadButton.Name = "VelReadButton";
+            this.VelReadButton.Size = new System.Drawing.Size(59, 28);
+            this.VelReadButton.TabIndex = 14;
+            this.VelReadButton.Text = "Read";
+            this.VelReadButton.UseVisualStyleBackColor = false;
+            this.VelReadButton.Click += new System.EventHandler(this.VelReadButton_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.VelocityPanel);
             this.Controls.Add(this.SerialPanel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "PID Tunner App";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
@@ -261,15 +311,16 @@
         private System.IO.Ports.SerialPort serialPort1;
         private System.Windows.Forms.Panel SerialPanel;
         private System.Windows.Forms.Label SerialComLabel;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox P_VelText;
+        private System.Windows.Forms.TextBox I_VelText;
+        private System.Windows.Forms.TextBox D_VelText;
         private System.Windows.Forms.Label PLabel;
         private System.Windows.Forms.Label ILabel;
         private System.Windows.Forms.Label DLabel;
         private System.Windows.Forms.Panel VelocityPanel;
         private System.Windows.Forms.Label VelocityLabel;
+        private System.Windows.Forms.Button VelReadButton;
+        private System.Windows.Forms.Button VelSendButton;
     }
 }
 
--- PID_Tuner_App/PID_Tuner_App/Form1.cs
+++ PID_Tuner_App/PID_Tuner_App/Form1.cs
@@ -23,13 +23,12 @@ namespace PID_Tuner_App
         private void Form1_Load(object sender, EventArgs e)
         {
             string[] ports = SerialPort.GetPortNames();
-            foreach (string port in ports)
-            {
-                PortMenu.Items.Add(port);
+            PortMenu.Items.AddRange(ports);
+            try{
                 PortMenu.SelectedIndex = 0;
             }
+            catch{}
             BaudRateMenu.SelectedIndex = 0;
-
             DisconnectButton.Visible = false;
         }
 
@@ -50,13 +49,13 @@ namespace PID_Tuner_App
         private void PortMenu_Click(object sender, EventArgs e)
         {
             string[] ports = SerialPort.GetPortNames();
-
             PortMenu.Items.Clear();
-            foreach (string port in ports)
-            { 
-                PortMenu.Items.Add(port);
+            PortMenu.Items.AddRange(ports);
+            try
+            {
                 PortMenu.SelectedIndex = 0;
             }
+            catch { }
         }
 
         private void BaudRateMenu_SelectedIndexChanged(object sender, EventArgs e)
@@ -70,18 +69,17 @@ namespace PID_Tuner_App
         private void ConnectButton_Click(object sender, EventArgs e)
         {   
             if (PortMenu.SelectedItem != null)
-            {
+            {   
                 ConnectButton.Visible = false;
                 DisconnectButton.Visible = true;
                 PortMenu.Enabled = false;
                 BaudRateMenu.Enabled = false;
                 ConnectButton.Enabled = false;
                 DisconnectButton.Enabled = true;
+                VelSendButton.Enabled = true;
+                VelReadButton.Enabled = true;
                 serialPort1.Open();
             }
-
-            Console.WriteLine(serialPort1.PortName);
-            Console.WriteLine(serialPort1.BaudRate);
         }
 
         private void DisconnectButton_Click(object sender, EventArgs e)
@@ -92,8 +90,48 @@ namespace PID_Tuner_App
             BaudRateMenu.Enabled = true;
             ConnectButton.Enabled = true;
             DisconnectButton.Enabled = false;
+            VelSendButton.Enabled = false;
+            VelReadButton.Enabled = false;
             serialPort1.Close();
             while (serialPort1.IsOpen) { }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string buffer;
+            char identByte;
+
+            buffer = serialPort1.ReadExisting();
+            var parts = buffer.Split(';');
+            identByte = Convert.ToChar(parts[0]);
+            Double Kp = Convert.ToDouble(parts[1]);
+            Double Ki = Convert.ToDouble(parts[2]);
+            Double Kd = Convert.ToDouble(parts[3]);
+
+            switch (identByte)
+            {
+                case 'v':
+                    P_VelText.Invoke((MethodInvoker)(() => P_VelText.Text = Kp.ToString("0.000")));
+                    I_VelText.Invoke((MethodInvoker)(() => I_VelText.Text = Ki.ToString("0.000")));
+                    D_VelText.Invoke((MethodInvoker)(() => D_VelText.Text = Kd.ToString("0.000")));
+                    break;
+            }
+        }
+
+        private void VelSendButton_Click(object sender, EventArgs e)
+        {
+            string buffer1 = String.Format("v;{0:0.000}", Convert.ToDouble(P_VelText.Text));
+            string buffer2 = String.Format(";{0:0.000}", Convert.ToDouble(I_VelText.Text));
+            string buffer3 = String.Format(";{0:0.000}\n", Convert.ToDouble(D_VelText.Text));
+            string buffer = buffer1 + buffer2 + buffer3;
+            serialPort1.Write(buffer);
+
+            Console.WriteLine(buffer);
+        }
+
+        private void VelReadButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("V\n");
         }
     }
 }
